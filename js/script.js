68ee2544026917fe5f1b6abded640f9896596f7f
Roman Amelin <skolakodu@gmail.com>
2024-10-26 04:24:24 +0600

    test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.prev',
+      prevEl: '.next',
+    },
+  
+
+  });
