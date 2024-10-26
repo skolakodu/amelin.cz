fdc89c9c5d0ff4e0df1ea3ee4748e4d07fed9bb1
Roman Amelin <skolakodu@gmail.com>
2024-10-26 12:42:07 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 3,
+    spaceBetween: 50,
     // Navigation arrows
     navigation: {
       nextEl: '.prev',
@@ -9,3 +10,41 @@ const swiper = new Swiper('.swiper', {
   
 
   });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const steps = document.querySelectorAll(".singleStep");
+
+  steps.forEach((step, index) => {
+    step.addEventListener("click", () => {
+      // Remove active class from all steps
+      steps.forEach(s => s.classList.remove("active"));
+      
+      // Add active class to the clicked step
+      step.classList.add("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const steps = document.querySelectorAll(".faq-item");
+
+  steps.forEach((step, index) => {
+    step.addEventListener("click", () => {
+      // Remove active class from all steps
+      steps.forEach(s => s.classList.remove("active"));
+      
+      // Add active class to the clicked step
+      step.classList.add("active");
+    });
+  });
+});
+
+window.addEventListener('scroll', () => {
+  const fixedElement = document.querySelector('.header');
+
+  if (window.scrollY > 0) {
+      fixedElement.classList.add('scrolled');
+  } else {
+      fixedElement.classList.remove('scrolled');
+  }
+});
